b92d23292536ed6925cb18f5de7c9da825f8766f
gviniciossalesp@gmail.com <gviniciossalesp@gmail.com>
2026-02-05 18:23:36 -0300

    ajustes!

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,26 @@
-const menuToggle = document.querySelector("#menu-toggle");
-const menuItems = document.querySelectorAll(".fancy-item");
+const html = document.documentElement;
+const themeToggle = document.querySelector('.l');
+const buttons = document.querySelectorAll('.btn')
 
-menuItems.forEach(item => {
-    item.addEventListener("click", () => {
-        menuToggle.checked = false; // fecha o menu
-    });
+// aplica tema salvo
+const savedTheme = localStorage.getItem("theme") || "light";
+html.setAttribute("data-theme", savedTheme);
+themeToggle.checked = savedTheme === "dark";
+
+themeToggle.addEventListener('change', function () {
+  const theme = this.checked ? "dark" : "light";
+  html.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
 });
+
+
+// clique no botão da navbar
+html.addEventListener('click', e => {
+    const el = e.target.closest('.btn');
+    if (!el) return;
+
+     buttons.forEach(b => b.classList.remove('active'));
+
+    el.classList.add('active');
+    removeActive();
+})
